356912556496595c2a3367e1dab239119e9d2c61
Matt Zamora <mzamora2899@gmail.com>
2022-07-07 20:18:25 -0600

    add gitignore and more sql queries

--- python-mysql/test.py
+++ python-mysql/test.py
@@ -1,17 +1,30 @@
 import mysql.connector
+from datetime import datetime
 
 db = mysql.connector.connect(
     host="localhost",
     user="root",
     database="test_schema",
+    passwd="Pass@123",
     auth_plugin='mysql_native_password'
 )
 
 mycursor = db.cursor()
 
+# Example of creating a Schema in MYSQL using python connector
+
 # mycursor.execute(
 # "CREATE TABLE Person (name VARCHAR(50), age smallint UNSIGNED, personID int PRIMARY KEY AUTO_INCREMENT)")
 
-mycursor.execute(
-    "INSERT INTO PERSON (name, age) VALUES (%s,%s)", ('matt', 22))
-db.commit()
+# Example of creating new object in schema and committing it to DB
+
+# mycursor.execute(
+#     "INSERT INTO PERSON (name, age) VALUES (%s,%s)", ('matt', 22))
+# db.commit()
+
+# mycursor.execute(
+# "CREATE TABLE Test (name varchar(50) NOT NULL, created datetime NOT NULL, gender ENUM('M', 'F', 'O') NOT NULL, id int PRIMARY KEY NOT NULL AUTO_INCREMENT)")
+
+# mycursor.execute("INSERT INTO TEST (name, created, gender) VALUES (%s, %s, %s)",
+#                  ("JACK", datetime.now(), "O"))
+# db.commit()
